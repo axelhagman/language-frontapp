6b4e7d81498e8e9054158246b8f623dca15cf0fe
axelhagman <axel.hagman@redeye.se>
2021-11-24 19:47:31 +0100

    init dev on basic practice

---
+++ pages/practice/[slug].js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BasicPractice = () => {
+  return (
+    <div>
+      <h1>Basic practice</h1>
+    </div>
+  );
+};
+
+export default BasicPractice;
